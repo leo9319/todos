cacc9dc6c2c0eb3a3cfb00bc0d59cc4a25c1d993
Leonard <leo_9319@yahoo.com>
2023-07-26 09:08:09 -0400

    saved image

--- login.php
+++ login.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 <div class="pt-24">
     <div class="flex bg-white rounded-lg shadow-lg overflow-hidden mx-auto max-w-sm lg:max-w-4xl">
-        <div class="hidden lg:block lg:w-1/2 bg-cover" style="background-image:url('https://images.unsplash.com/photo-1546514714-df0ccc50d7bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80')"></div>
+        <div class="hidden lg:block lg:w-1/2 bg-cover" style="background-image:url('/public/images/login.jpg')"></div>
         <div class="w-full p-8 lg:w-1/2">
             <form method="post" action="login.php">
                 <h2 class="text-2xl font-semibold text-gray-700 text-center">To Do App</h2>
